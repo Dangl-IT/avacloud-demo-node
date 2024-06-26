a8222078782649114774c9089bdf850c91cd376a
Georg Dangl <georg@dangl.me>
2024-06-26 14:56:25 +0200

    Use fetch in token requests instead of third party dependency

--- utils.ts
+++ utils.ts
@@ -1,5 +1,3 @@
-import { post } from "request";
-
 export async function getOAuth2AccessToken(
   clientId: string,
   clientSecret: string,
@@ -11,34 +9,24 @@ export async function getOAuth2AccessToken(
     );
     throw new Error("Missing clientId or clientSecret");
   }
-  const clientCredentialsRequest = new Promise(function (resolve, reject) {
-    post(
-      identityTokenUrl,
-      {
-        auth: {
-          username: clientId,
-          password: clientSecret,
-        },
-        body: "grant_type=client_credentials&scope=avacloud",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      },
-      function (err, resp, body) {
-        if (err) {
-          console.log("Error");
-          reject(err);
-        } else {
-          resolve(body);
-        }
-      }
-    );
-  });
   try {
-    const clientCredentialsResult = await clientCredentialsRequest;
-    const accessToken = JSON.parse(<string>clientCredentialsResult)[
-      "access_token"
-    ];
+    const tokenResponseRaw = await fetch(identityTokenUrl, {
+      method: "POST",
+      body: "grant_type=client_credentials&scope=avacloud",
+      headers: {
+        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (tokenResponseRaw.status !== 200) {
+      throw new Error(
+        "Failed to obtain an access token, status code: " +
+          tokenResponseRaw.status
+      );
+    }
+
+    const jsonResponse = await tokenResponseRaw.json();
+    const accessToken = jsonResponse["access_token"];
     if (!accessToken) {
       console.log(
         "Failed to obtain an access token. Have you read the documentation and set up your OAuth2 client?"
